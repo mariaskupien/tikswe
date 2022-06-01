3fe9cf12f6dc3bd2b45951cc261e5e04d5fe713e
mariaskupien <104564675+mariaskupien@users.noreply.github.com>
2022-06-01 21:19:05 +0200

    Add files via upload

---
+++ licznik.js
@@ -0,0 +1,17 @@
+﻿var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+let isCounterIncremented = sessionStorage.getItem('isCounterIncremented');
+if(!isCounterIncremented){
+    if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+  } else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+  }
+counterContainer.innerHTML = visitCount;
+ sessionStorage.setItem('isCounterIncremented',true);
+}
+  
